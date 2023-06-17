dc992b17d88836350acb339f5cd4ea02b0765230
charburgx <charburgx@gmail.com>
2023-06-17 15:51:57 -0700

    multi-threaded frame pacing

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,13 @@
 use core::panic;
-use std::time::Duration;
+use std::{
+    sync::{
+        mpsc::{channel, TryRecvError},
+        Arc, RwLock,
+    },
+    time::Duration,
+};
 
+use futures::future::pending;
 use winit::{
     event::{ElementState, Event, KeyboardInput, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -11,25 +18,45 @@ use crate::{
     element::RootConstructor,
     input::{input_state::InputState, winit::WinitState},
     scene::scene::Scene,
-    surface::RenderSurface,
+    surface::{RenderAttachment, RenderSurface},
 };
 
 pub struct App<Root: RootConstructor + 'static> {
     window: winit::window::Window,
 
-    render_surface: RenderSurface,
+    render_surface: Arc<RwLock<RenderSurface>>,
     scene: Scene<Root>,
 
     winit_state: WinitState,
     input_state: InputState,
+
+    pending_render_request: bool,
+    queued_reize: Option<(winit::dpi::PhysicalSize<u32>, Option<f64>)>,
 }
 
 impl<Root: RootConstructor + 'static> App<Root> {
     pub fn run(mut self, event_loop: EventLoop<()>) {
         use std::time::*;
 
-        let mut last_render_duration: Option<Duration> = None;
-        let mut last_render_time: Option<Instant> = None;
+        let (request_render_send, request_render_recv) = channel::<()>();
+        let (render_output_send, render_output_recv) =
+            channel::<Result<RenderAttachment, wgpu::SurfaceError>>();
+
+        let rthread_rs = self.render_surface.clone();
+
+        std::thread::spawn(move || loop {
+            match request_render_recv.recv() {
+                Ok(_) => {
+                    render_output_send
+                        .send(rthread_rs.write().unwrap().get_output())
+                        .unwrap();
+                }
+                Err(e) => {
+                    log::error!("render swapchain thread failed: {}", e);
+                    return;
+                }
+            }
+        });
 
         event_loop.run(move |event, _, control_flow| {
             match event {
@@ -61,66 +88,65 @@ impl<Root: RootConstructor + 'static> App<Root> {
                     }
                 },
                 Event::RedrawRequested(window_id) if window_id == self.window.id() => {
-                    let mut do_render = true;
+                    let render_start_time = Instant::now();
 
-                    if let (Some(last_render_duration), Some(last_render_time)) =
-                        (last_render_duration, last_render_time)
-                    {
-                        if let Some(frame_time) = get_window_frame_time(&self.window) {
-                            let elapsed_time = last_render_time.elapsed();
-
-                            let buffer_duration = last_render_duration + Duration::from_micros(0);
-
-                            if elapsed_time < (frame_time.saturating_sub(buffer_duration)) {
-                                do_render = false;
-                            }
-                        }
+                    if !self.pending_render_request {
+                        request_render_send.send(()).unwrap();
+                        self.pending_render_request = true;
+                        return;
                     }
 
-                    if do_render {
-                        let render_start_time = Instant::now();
-
-                        let texture_block_start = Instant::now();
-                        // let output = self.render_surface.surface().get_current_texture();
-                        let output = self.render_surface.get_output();
-                        let texture_block_time = texture_block_start.elapsed();
-                        // log::trace!("texture block time: {:?}", texture_block_time);
-
-                        match output {
-                            Ok(output) => {
-                                let start = Instant::now();
-
-                                let raw_input = self.winit_state.take_egui_input();
-
-                                let input_state = std::mem::take(&mut self.input_state)
-                                    .begin_frame(raw_input, true);
-
-                                let (result_input, result_output) =
-                                    self.scene.render(&self.render_surface, output, input_state);
-
-                                self.input_state = result_input;
-
-                                self.winit_state
-                                    .handle_platform_output(&self.window, result_output);
-
-                                last_render_time = Some(start);
-                                last_render_duration = Some(start.elapsed());
-                            }
-                            // Reconfigure the surface if lost
-                            Err(wgpu::SurfaceError::Lost) => self
-                                .render_surface
-                                .reconfigure(self.scene.get_dependents_mut()),
-                            // The system is out of memory, we should probably quit
-                            Err(wgpu::SurfaceError::OutOfMemory) => panic!("out of memory"),
-                            // All other errors (Outdated, Timeout) should be resolved by the next frame
-                            Err(e) => eprintln!("{:?}", e),
+                    let output = match render_output_recv.try_recv() {
+                        Ok(output) => {
+                            self.pending_render_request = false;
+                            output
                         }
+                        Err(TryRecvError::Empty) => return,
+                        Err(TryRecvError::Disconnected) => {
+                            panic!("output swapchain thread disconnected")
+                        }
+                    };
 
-                        let render_time = render_start_time.elapsed();
-                        // log::trace!("render_time: {:?}", render_time);
+                    // let output = self.render_surface.get_output();
+
+                    match output {
+                        Ok(output) => {
+                            let raw_input = self.winit_state.take_egui_input();
+
+                            let input_state =
+                                std::mem::take(&mut self.input_state).begin_frame(raw_input, true);
+
+                            let (result_input, result_output) = self.scene.render(
+                                &self.render_surface.read().unwrap(),
+                                output,
+                                input_state,
+                            );
+
+                            self.input_state = result_input;
+
+                            self.winit_state
+                                .handle_platform_output(&self.window, result_output);
+                        }
+                        // Reconfigure the surface if lost
+                        Err(wgpu::SurfaceError::Lost) => self
+                            .render_surface
+                            .write()
+                            .unwrap()
+                            .reconfigure(self.scene.get_dependents_mut()),
+                        // The system is out of memory, we should probably quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => panic!("out of memory"),
+                        // All other errors (Outdated, Timeout) should be resolved by the next frame
+                        Err(e) => eprintln!("{:?}", e),
                     }
+
+                    let render_time = render_start_time.elapsed();
+                    log::trace!("render_time: {:?}", render_time);
                 }
                 Event::MainEventsCleared => {
+                    if let Some((size, scale_fac)) = self.queued_reize.take() {
+                        self.resize(size, scale_fac);
+                    }
+
                     // RedrawRequested will only trigger once, unless we manually
                     // request it.
                     self.window.request_redraw()
@@ -151,17 +177,28 @@ impl<Root: RootConstructor + 'static> App<Root> {
         Self {
             window,
 
-            render_surface,
+            render_surface: Arc::new(render_surface.into()),
             scene,
 
             winit_state,
             input_state,
+
+            pending_render_request: false,
+            queued_reize: None,
         }
     }
 
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>, scale_factor: Option<f64>) {
-        self.render_surface
-            .resize(new_size, scale_factor, self.scene.get_dependents_mut());
+        if self.pending_render_request {
+            self.queued_reize = Some((new_size, scale_factor));
+            return;
+        }
+
+        self.render_surface.write().unwrap().resize(
+            new_size,
+            scale_factor,
+            self.scene.get_dependents_mut(),
+        );
     }
 }
 
